fac657c91ad134c6445a8d428a7824a68cb110f7
MATT GARNETT <MATT@MATTGARNETT.COM>
2015-10-03 16:35:33 -0700

    updating

--- db/schema.rb
+++ db/schema.rb
@@ -14,13 +14,16 @@
 ActiveRecord::Schema.define(version: 20151002235236) do
 
   create_table "posts", force: :cascade do |t|
-    t.string   "uid"
     t.string   "ig_body"
     t.string   "media"
     t.string   "tweet_body"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "posts", ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "provider"
