f2dd74a772f76a6410e30b242b9878502040af84
MATT GARNETT <MATT@MATTGARNETT.COM>
2015-10-03 15:24:56 -0700

    Updating

--- app/controllers/transfer_controller.rb
+++ app/controllers/transfer_controller.rb
@@ -3,9 +3,9 @@ class TransferController < ApplicationController
 
   def index
     render plain: "HELLO THERE FRIENDS"
-    if params[0]["_json"]["data"]["media_id"]
+    if params["_json"][0]["data"]["media_id"]
 
-      Post.create_post_from_media_id(params[0]["_json"]["data"]["media_id"])
+      Post.create_post_from_media_id(params["_json"][0]["data"]["media_id"])
 
     end
   end
