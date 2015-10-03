94339425151894b4e7829660d33a6f97a8519955
MATT GARNETT <MATT@MATTGARNETT.COM>
2015-10-03 12:42:27 -0700

    updated transfer controller

--- app/controllers/transfer_controller.rb
+++ app/controllers/transfer_controller.rb
@@ -1,6 +1,6 @@
 class TransferController < ApplicationController
   def index
-    puts "HELLO THERE FRIENDS"
+    render plain: "HELLO THERE FRIENDS"
     #puts params["data"]
     #puts params["_json"]["data"]["media_id"]
     #puts params
