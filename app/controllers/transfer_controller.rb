fb4263ac1096c597fd50297f741bbb7df4fcece7
MATT GARNETT <MATT@MATTGARNETT.COM>
2015-10-03 15:45:34 -0700

    Updating

--- app/controllers/transfer_controller.rb
+++ app/controllers/transfer_controller.rb
@@ -3,11 +3,13 @@ class TransferController < ApplicationController
 
   def index
 
-    if params["_json"][0]["data"]["media_id"]
+    if params['_json'][0]['data']['media_id']
 
-      #media_id = params["_json"][0]["data"]["media_id"]
+      media_id = params['_json'][0]['data']['media_id']
 
-      Post.create_post_from_media_id()
+      puts media_id
+
+      Post.create_post_from_media_id(media_id)
 
     end
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,9 +4,9 @@ class Post < ActiveRecord::Base
   belongs_to :user
   #validates :user_id, presence: true
 
-  def self.create_post_from_media_id()
+  def self.create_post_from_media_id(id)
 
-    data = Net::HTTP.get_response(URI.parse("https://api.instagram.com/v1/media/" + params["_json"][0]["data"]["media_id"] + "client_id=2954598143aa4dbba123da8a68de1466")).body
+    data = Net::HTTP.get_response(URI.parse("https://api.instagram.com/v1/media/" + id + "client_id=2954598143aa4dbba123da8a68de1466")).body
 
     user = User.where(instagram_uid: data["json"][0]["data"]["user"]["id"])
     user.post.create(
