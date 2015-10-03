f11a56e8f5e09fae146f44ca060205984620618b
MATT GARNETT <MATT@MATTGARNETT.COM>
2015-10-03 12:44:38 -0700

    updated transfer controller

--- app/controllers/transfer_controller.rb
+++ app/controllers/transfer_controller.rb
@@ -1,4 +1,6 @@
 class TransferController < ApplicationController
+  skip_before_filter  :verify_authenticity_token
+  
   def index
     render plain: "HELLO THERE FRIENDS"
     #puts params["data"]
