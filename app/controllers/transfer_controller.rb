741f5b2a44f9a0072cb48e408f7549a30469cc48
MATT GARNETT <MATT@MATTGARNETT.COM>
2015-10-03 15:36:03 -0700

    Updating

--- app/controllers/transfer_controller.rb
+++ app/controllers/transfer_controller.rb
@@ -2,12 +2,12 @@ class TransferController < ApplicationController
   skip_before_filter  :verify_authenticity_token
 
   def index
-    
+
     if params["_json"][0]["data"]["media_id"]
 
-      media_id = params["_json"][0]["data"]["media_id"]
+      #media_id = params["_json"][0]["data"]["media_id"]
 
-      Post.create_post_from_media_id(media_id)
+      Post.create_post_from_media_id()
 
     end
   end
