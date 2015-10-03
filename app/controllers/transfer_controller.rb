4b9d1d55cc73542de31efaf4c73a250c11b57d75
MATT GARNETT <MATT@MATTGARNETT.COM>
2015-10-03 15:29:37 -0700

    Updating

--- app/controllers/transfer_controller.rb
+++ app/controllers/transfer_controller.rb
@@ -2,10 +2,12 @@ class TransferController < ApplicationController
   skip_before_filter  :verify_authenticity_token
 
   def index
-    render plain: "HELLO THERE FRIENDS"
+    
     if params["_json"][0]["data"]["media_id"]
 
-      Post.create_post_from_media_id(params["_json"][0]["data"]["media_id"])
+      media_id = params["_json"][0]["data"]["media_id"]
+
+      Post.create_post_from_media_id(media_id)
 
     end
   end
