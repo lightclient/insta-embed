791ea0e302489f44ef47bc6bf3cc1572d352d199
MATT GARNETT <MATT@MATTGARNETT.COM>
2015-10-03 16:53:47 -0700

    updating

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,16 +9,17 @@ class Post < ActiveRecord::Base
     json_object = Net::HTTP.get_response(URI.parse("https://api.instagram.com/v1/media/" + id + "/?client_id=2954598143aa4dbba123da8a68de1466")).body
 
     data = JSON.parse(json_object)
-    
+
     puts data
     puts "--------------------------------------------"
     puts data["data"]["user"]["id"]
 
     user = User.where(instagram_uid: data["data"]["user"]["id"])
-    user.post.create(
+    Post.create(
       ig_body: data["data"]["caption"]["text"],
       media: data["data"]["images"]["standard_resolution"]["url"],
-      tweet_body: data["data"]["caption"]["text"]
+      tweet_body: data["data"]["caption"]["text"],
+      user_id: user.id
     )
     user
   end
