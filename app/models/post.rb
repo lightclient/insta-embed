672895a19112d61ed66674047386e5ec4e643385
MATT GARNETT <MATT@MATTGARNETT.COM>
2015-10-03 23:42:18 -0700

    updating

--- app/models/post.rb
+++ app/models/post.rb
@@ -40,7 +40,7 @@ class Post < ActiveRecord::Base
   end
 
   def post_to_twitter
-    user.twitter.update_with_media(tweet_body, open(URI.parse(media)))
+    user.twitter.update_with_media(tweet_body, open(URI.parse(media)).read)
   end
 
 end
