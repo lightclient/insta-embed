bc9dc6dc86b75ababac71d7e208de5cfe7632083
MATT GARNETT <MATT@MATTGARNETT.COM>
2015-10-04 12:05:14 -0700

    Fixing iPhone width issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,8 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//= require bootstrap-dropdown
+//= require bootstrap
+//= require jquery
+//= require jquery-ui
+//= require jquery_ujs
